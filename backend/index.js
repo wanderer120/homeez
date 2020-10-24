3dc92298c2656aaaed800613c0d1ccf052a1dc0a
wanderer120 <megamanx120@hotmail.com>
2020-10-24 20:12:51 +0800

    test

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const config = require('./config/main.json')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./queries')
+
+process.on('uncaughtException', function (err) {
+  console.error(err);
+});
+
+const app = express()
+
+
+const port = config.port
+
+var rawBodySaver = function (req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || 'utf8');
+  }
+}
+app.use(bodyParser.json({ verify: rawBodySaver }));
+app.use(bodyParser.urlencoded({ verify: rawBodySaver, extended: true }));
+app.use(bodyParser.raw({ verify: rawBodySaver, type: function () { return true } }));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'yo!' })
+})
+
+app.get('/getPlayers', db.getPlayers)
+app.get('/getWonderpotByAgent/:agentId', db.getWonderpotByAgent)
+app.put('/addWonderpotByAgent/:agentId', db.addWonderpotByAgent)
+
+app.use(function(error, req, res, next) {
+  // Will get here
+  res.json({ message: error.message });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
